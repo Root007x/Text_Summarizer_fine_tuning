fe4127889d9773e66d61704c2778d34ad4c8a995
Root007x <mahadihasanpm@gmail.com>
2026-01-22 22:36:11 +0600

    data ingestion pipeline created

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import (
+    DataIngestionPipeline,
+)
 
-logger.info("Starting the text summarization process.")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_ingestion_pipeline = DataIngestionPipeline()
+    data_ingestion_pipeline.init_data_ingestion()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\n")
+except Exception as e:
+    logger.exception(f"Error in stage {STAGE_NAME}: {e}")
+    raise e
